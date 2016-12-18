2f0276bff1e3b07c13d657d330de321c5c131132
AJTrowell <ajtrowell@gmail.com>
2016-12-18 01:54:54 -0600

    Add procedural asteroid generation, not randomized.
    Asteroids are now built out of vertices in a procedure.
    These could be manipulated randomly to create uniqe 
    asteroids.

--- Asteroid.js
+++ Asteroid.js
@@ -6,13 +6,16 @@ function Asteroid() {
   // Random velocity heading (0,360)
   this.vel = p5.Vector.fromAngle(radians(random(360)));
   // Random velocity magnitude
-  this.vel.mult(random(1,4  ))
+  this.vel.mult(random(1,4))
 
-  this.r = 40;
-  this.heading = 0; // degrees CW from vertical.
-
+  this.heading = random(360); // degrees CW from vertical.
   // spin rate
   this.rotationRate = random(-3,3);
+  // Size
+  this.r = 40; // nominal radius
+  this.numVertices = 10; // number of points in asteroid perimeter.
+  this.perimeter = []; // Shape of asteroid, array of vectors.
+  this.generate(); // Generate procedural asteroid shape array.
 }
 Asteroid.prototype.render = function() {
   push();
@@ -20,9 +23,23 @@ Asteroid.prototype.render = function() {
   stroke(255);
   translate(this.pos.x,this.pos.y);
   rotate(this.heading * PI / 180);
-  // Nead a fancy asteroid drawing method
-  ellipse(0,0, this.r*2, this.r*2);
+  //ellipse(0,0, this.r*2, this.r*2.5);
+  // Fancy asteroid drawing method
+  beginShape();
+  for(let point of this.perimeter) {
+    vertex(point.x,point.y);
+  }
+  endShape(CLOSE);
   pop();
+}
+Asteroid.prototype.generate = function() {
+  // Procedurally generate a new asteroid.
+  for(var i = 0; i < this.numVertices; i++) {
+    let angle = map(i, 0, this.numVertices, 0, TWO_PI);
+    let x = this.r*cos(angle);
+    let y = this.r*sin(angle);
+    this.perimeter[i]=createVector(x,y);
+  }
 }
 Asteroid.prototype.turn = function(angle) {
   this.heading += angle;
